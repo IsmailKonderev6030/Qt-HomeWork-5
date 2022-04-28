a12c2b9b4eb92102aa9a17730139ad388a6d70d1
IsmailKonderev6030 <73698568+IsmailKonderev6030@users.noreply.github.com>
2022-04-28 23:06:55 +0300

    Add files via upload

---
+++ build-untitled-Desktop_Qt_6_2_4_MinGW_64_bit-Debug/switch_ru.ts
@@ -0,0 +1,143 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>HotKeys</name>
+    <message>
+        <location filename="hotkeys.ui" line="14"/>
+        <location filename="hotkeys.cpp" line="89"/>
+        <source>Dialog</source>
+        <translation>Диалоговое окно</translation>
+    </message>
+    <message>
+        <location filename="hotkeys.ui" line="79"/>
+        <location filename="hotkeys.ui" line="92"/>
+        <location filename="hotkeys.ui" line="105"/>
+        <location filename="hotkeys.ui" line="118"/>
+        <location filename="hotkeys.cpp" line="91"/>
+        <location filename="hotkeys.cpp" line="92"/>
+        <location filename="hotkeys.cpp" line="93"/>
+        <location filename="hotkeys.cpp" line="94"/>
+        <source>CTRL +</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hotkeys.cpp" line="12"/>
+        <location filename="hotkeys.cpp" line="95"/>
+        <source>Save</source>
+        <translation>Сохранить</translation>
+    </message>
+    <message>
+        <location filename="hotkeys.cpp" line="13"/>
+        <location filename="hotkeys.cpp" line="20"/>
+        <location filename="hotkeys.cpp" line="96"/>
+        <location filename="hotkeys.cpp" line="100"/>
+        <source>Exit</source>
+        <translation>Выйти</translation>
+    </message>
+    <message>
+        <location filename="hotkeys.cpp" line="17"/>
+        <location filename="hotkeys.cpp" line="97"/>
+        <source>Open file</source>
+        <translation>Открыть файл</translation>
+    </message>
+    <message>
+        <location filename="hotkeys.cpp" line="18"/>
+        <location filename="hotkeys.cpp" line="98"/>
+        <source>Save file</source>
+        <translation>Сохрнаить файл</translation>
+    </message>
+    <message>
+        <location filename="hotkeys.cpp" line="19"/>
+        <location filename="hotkeys.cpp" line="99"/>
+        <source>New file</source>
+        <translation>Создать файл</translation>
+    </message>
+    <message>
+        <location filename="hotkeys.cpp" line="21"/>
+        <location filename="hotkeys.cpp" line="101"/>
+        <source>Enter only one symbol in ENG</source>
+        <translation>Введите букву строго на англ</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>Текстовый редактор</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="23"/>
+        <source>Light</source>
+        <translation>Светлый</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="24"/>
+        <source>Dark</source>
+        <translation>Темный</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="26"/>
+        <location filename="mainwindow.cpp" line="163"/>
+        <source>Text editer</source>
+        <translation>Текстовый редактор</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="27"/>
+        <location filename="mainwindow.cpp" line="165"/>
+        <source>Open file</source>
+        <translation>Открыть файл</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="28"/>
+        <location filename="mainwindow.cpp" line="166"/>
+        <source>Save file</source>
+        <translation>Сохранить файл</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="29"/>
+        <location filename="mainwindow.cpp" line="167"/>
+        <source>Information</source>
+        <translation>Информация</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="30"/>
+        <location filename="mainwindow.cpp" line="168"/>
+        <source>Hotkeys</source>
+        <translation>Горячие клавиши</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="31"/>
+        <location filename="mainwindow.cpp" line="169"/>
+        <source>Only read</source>
+        <translation>Только для чтения</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="32"/>
+        <location filename="mainwindow.cpp" line="170"/>
+        <source>Language</source>
+        <translation>Язык</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="44"/>
+        <source>Info</source>
+        <translation>Инфо</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="62"/>
+        <source>Select file txt</source>
+        <translation>Выберите файл txt</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="75"/>
+        <source>Save file txt</source>
+        <translation>Сохраните файл txt</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="151"/>
+        <source>Create file txt</source>
+        <translation>Создайте файл txt</translation>
+    </message>
+</context>
+</TS>
---
+++ build-untitled-Desktop_Qt_6_2_4_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -0,0 +1,134 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.2.4
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QCheckBox>
+#include <QtWidgets/QComboBox>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPlainTextEdit>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QGridLayout *gridLayout;
+    QPushButton *pushButton_4;
+    QPushButton *pushButton;
+    QPushButton *pushButton_3;
+    QPushButton *pushButton_2;
+    QLabel *label;
+    QPlainTextEdit *plainTextEdit;
+    QComboBox *comboBox;
+    QCheckBox *checkBox;
+    QComboBox *comboBox_2;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(626, 431);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        gridLayout = new QGridLayout(centralwidget);
+        gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
+        pushButton_4 = new QPushButton(centralwidget);
+        pushButton_4->setObjectName(QString::fromUtf8("pushButton_4"));
+
+        gridLayout->addWidget(pushButton_4, 3, 3, 1, 1);
+
+        pushButton = new QPushButton(centralwidget);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+
+        gridLayout->addWidget(pushButton, 2, 1, 1, 1);
+
+        pushButton_3 = new QPushButton(centralwidget);
+        pushButton_3->setObjectName(QString::fromUtf8("pushButton_3"));
+
+        gridLayout->addWidget(pushButton_3, 2, 3, 1, 1);
+
+        pushButton_2 = new QPushButton(centralwidget);
+        pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
+
+        gridLayout->addWidget(pushButton_2, 2, 2, 1, 1);
+
+        label = new QLabel(centralwidget);
+        label->setObjectName(QString::fromUtf8("label"));
+        QFont font;
+        font.setPointSize(10);
+        label->setFont(font);
+
+        gridLayout->addWidget(label, 4, 3, 1, 1);
+
+        plainTextEdit = new QPlainTextEdit(centralwidget);
+        plainTextEdit->setObjectName(QString::fromUtf8("plainTextEdit"));
+
+        gridLayout->addWidget(plainTextEdit, 0, 0, 1, 4);
+
+        comboBox = new QComboBox(centralwidget);
+        comboBox->setObjectName(QString::fromUtf8("comboBox"));
+
+        gridLayout->addWidget(comboBox, 5, 3, 1, 1);
+
+        checkBox = new QCheckBox(centralwidget);
+        checkBox->setObjectName(QString::fromUtf8("checkBox"));
+
+        gridLayout->addWidget(checkBox, 2, 0, 1, 1);
+
+        comboBox_2 = new QComboBox(centralwidget);
+        comboBox_2->setObjectName(QString::fromUtf8("comboBox_2"));
+
+        gridLayout->addWidget(comboBox_2, 5, 0, 1, 1);
+
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 626, 17));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        pushButton_4->setText(QString());
+        pushButton->setText(QString());
+        pushButton_3->setText(QString());
+        pushButton_2->setText(QString());
+        label->setText(QString());
+        checkBox->setText(QString());
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
